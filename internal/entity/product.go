e48ad25361b6f2673b2c14ff80365770e64a444d
Osmar L. Villaba <osmarvillalbagonzalez@gmail.com>
2024-01-30 11:22:01 -0300

    FIX: entity notation category and product add notation to struct to personalize the name when parse to JSON

--- internal/entity/product.go
+++ internal/entity/product.go
@@ -3,12 +3,12 @@ package entity
 import "github.com/google/uuid"
 
 type Product struct {
-	ID          string
-	Name        string
-	Description string
-	Price       float64
-	CategoryID  string
-	ImageURL    string
+	ID          string  `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+	CategoryID  string  `json:"category_id"`
+	ImageURL    string  `json:"image_url"`
 }
 
 func NewProduct(
